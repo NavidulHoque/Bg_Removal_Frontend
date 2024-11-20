65c324b1ab7a58019cc125460d28f9480bdaed5d
navidul hoque <hnavidul@gmail.com>
2024-11-20 18:19:41 +0600

    frontend

--- auth.ts
+++ auth.ts
@@ -20,17 +20,43 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
 
                 if (response.data.status) {
-                    return response.data.user   
-                } 
-                
+                    return response.data.user
+                }
+
                 else {
                     throw new Error(response.data.message)
                 }
-            } 
-            
+            }
+
             catch (error) {
                 throw new Error(error as string);
             }
         }
     })],
+
+    // callbacks: {
+    //     async signIn({ user, account, profile }) {
+    //         // Handle different types of sign-ins
+    //         if (account.provider === "credentials") {
+    //             // User signed in using credentials
+    //             return !!user; // Allow sign-in if a user was returned by `authorize`
+    //         }
+
+    //         else {
+    //             // User signed in using OAuth provider
+    //             try {
+    //                 await axios.post(`${process.env.EXPRESS_BACKEND_URL}/api/users`, {
+    //                     email: user.email,
+    //                     name: user.name,
+    //                     image: user.image,
+    //                     provider: account.provider,
+    //                 });
+    //                 return true;
+    //             } catch (error) {
+    //                 console.error("OAuth user creation error:", error);
+    //                 return false;
+    //             }
+    //         }
+    //     },
+    // }
 })
