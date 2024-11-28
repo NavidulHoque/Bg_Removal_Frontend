7c614da555144f2398d06f92637d059c29a4d45d
navidul hoque <hnavidul@gmail.com>
2024-11-28 19:40:51 +0600

    frontend

--- app/AppProvider.tsx
+++ app/AppProvider.tsx
@@ -1,9 +1,16 @@
+import ImageProvider from "@/context/ImageContext";
 import UserCreditsProvider from "@/context/UserCreditsContext";
 
 export default function AppProvider({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <UserCreditsProvider>
-        { children }
-    </UserCreditsProvider>
-  )
+    return (
+        <UserCreditsProvider>
+
+            <ImageProvider>
+
+                {children}
+
+            </ImageProvider>
+            
+        </UserCreditsProvider>
+    )
 }
